c18f151bc92d7700eb37ef1e4e5dbcf721ee8686
G-T-O <38909596+G-T-O@users.noreply.github.com>
2020-02-24 20:28:30 +0100

    Create vertex Class

---
+++ Shortest path/Dijkstra/Vertex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dijkstra
+{
+    class Vertex
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Name { get; set; }
+        public Vertex(int x,int y,string name)
+        {
+            X = x;
+            Y = y;
+            Name = name;
+        }
+    }
+}
