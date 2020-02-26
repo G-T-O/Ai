15c6ac6d0a165dff97c6dab50717b9ccdeffd32a
G-T-O <38909596+G-T-O@users.noreply.github.com>
2020-02-26 21:29:04 +0100

    Creating the graph we are working on and the basic of dijkstra

--- Shortest path/Dijkstra/Program.cs
+++ Shortest path/Dijkstra/Program.cs
@@ -7,7 +7,42 @@ namespace Dijkstra
     {
         static void Main(string[] args)
         {
+            Vertex[] vertexArray = new Vertex[]{
+                new Vertex(5, 3, "Vertex A") ,
+                new Vertex(8, 9, "Vertex B") ,
+                new Vertex(1, 7, "Vertex C") ,
+                new Vertex(14, 2,"Vertex D") };
+            //  A  B  C  D
+            int[,] graph = new int[,] {{ 0, 4, 0, 0 },
+                                       { 4, 0, 5, 8 },
+                                       { 0, 4, 0, 2 },
+                                       { 0, 8, 2, 0 }};
 
+
+            const int START = 0; // The index of the vertex who will be used 
+            const int END = 4; // The index of the vertex we are looking for 
+            int numberOfVertex = vertexArray.Length;
+
+
+            int[] minimumWeight = new int[numberOfVertex];
+            Vertex[] precursor = new Vertex[numberOfVertex];
+
+            List<Vertex> hasNotBeenVisited = new List<Vertex>();
+            hasNotBeenVisited.AddRange(vertexArray);
+
+           
+            for (int i = 0; i < numberOfVertex; i++)
+            {
+                minimumWeight[i] = int.MaxValue;
+                precursor[i] = null;
+            }
+            minimumWeight[START] = 0;
+
+
+
+           
         }
     }
 }
+
+
